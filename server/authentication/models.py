508dee92b07bafe54a9a51861a31820c9cbde03e
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-24 19:10:13 +0500

    added user detail model

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    
+class UserProfile(models.Model):
+    USER_TYPES = [
+        ('admin', 'Admin'),
+        ('seller', 'Seller'),
+        ('buyer', 'Buyer'),
+    ]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='buyer')
+
+    def __str__(self):
+        return self.user.username
+    
