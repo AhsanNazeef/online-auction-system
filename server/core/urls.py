2e254150187222e010f6fabe423f910a7b701719
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-24 19:13:46 +0500

    added auth and debug urls

--- server/core/urls.py
+++ server/core/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+import debug_toolbar
+
+admin.site.site_header = 'Online Auction System Admin'
+admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
