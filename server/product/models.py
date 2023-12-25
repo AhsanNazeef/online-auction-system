f181c1630a3d5ee808b72fde692f0c9d17fad969
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2023-12-25 21:55:55 +0500

    created model for product images

--- server/product/models.py
+++ server/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Product(models.Model):
     STATUS_CHOICES = [
         ('draft', 'Draft'),
@@ -8,12 +9,23 @@ class Product(models.Model):
         ('sold', 'Sold'),
         ('delivered', 'Delivered'),
     ]
-    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100)
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     minimum_bid = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='live')
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.product_name
+        return self.name
 
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.TextField()
+
+    def __str__(self):
+        return f"Image for Product: {self.product.name}"
