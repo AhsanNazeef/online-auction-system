394e301d8b6f0337f8c506ee081e1efa33b1cc40
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-01-01 16:39:48 +0500

    added bids api

--- server/bid/views.py
+++ server/bid/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.pagination import DefaultPagination
+from .serializers import BidSerializer
+from .models import Bid
+
+
+class BidViewSet(ModelViewSet):
+    http_method_names = ['get', 'post']
+    serializer_class = BidSerializer
+    pagination_class = DefaultPagination
+    
+    def get_serializer_context(self):
+        return {'auction_id': self.kwargs['auction_pk'], 'user_id' : self.request.user.id}
+
+    def get_queryset(self):
+        return Bid.objects \
+                .filter(auction_id=self.kwargs['auction_pk']) \
+                .select_related('auction')
+          
