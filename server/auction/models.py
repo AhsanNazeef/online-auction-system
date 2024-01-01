aa2a2054ab93709b0d9a4fe262e88db569652868
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-01-01 11:01:46 +0500

    added auction api

--- server/auction/models.py
+++ server/auction/models.py
@@ -3,11 +3,18 @@ from product.models import Product
 
 
 class Auction(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('draft', 'Draft'),
+        ('live', 'Live'),
+        ('closed', 'Closed'),
+    ]
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    minimum_bid = models.DecimalField(max_digits=10, decimal_places=2)
-    auto_bidding_enabled = models.BooleanField(default=False)
-
+    minimum_bid = models.PositiveIntegerField()
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='pending')
+    
     def __str__(self):
         return f"Auction for {self.product.product_name}"
--- server/auction/views.py
+++ server/auction/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.pagination import DefaultPagination
+from .serializers import AuctionSerializer, CreateAuctionSerializer, UpdateAuctionSerializer
+from .models import Auction
+
+
+class AuctionViewSet(ModelViewSet):
+    queryset = Auction.objects.all()
+    pagination_class = DefaultPagination
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateAuctionSerializer
+        if self.request.method in ['PUT', 'PATCH']:
+            return UpdateAuctionSerializer
+        return AuctionSerializer           
