2910216ebc3818d06f22f157e4b1b82966167c56
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-01-05 11:41:01 +0500

    added layouts

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
           Page not found
         </h1>
         <p className="mt-4 text-base leading-7 text-gray-600">
-          Sorry, we couldn&apos:t find the page you&apos;re looking for.
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
         <div className="mt-4 flex items-center justify-center gap-x-3">
           <Link href="/"
