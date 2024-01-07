553d846e69894b114650080ec4c3fc1eb208f418
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-01-07 15:22:39 +0500

    adding cors on BE

--- server/core/settings.py
+++ server/core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'debug_toolbar',
     'authentication',
@@ -71,7 +72,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 ROOT_URLCONF = 'core.urls'
 
