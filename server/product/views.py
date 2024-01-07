16d2a877cb3589b7b1b04542b80a65c225b9fb57
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-01-07 15:22:22 +0500

    adding filtering of products on BE

--- server/product/views.py
+++ server/product/views.py
@@ -1,14 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 
 from core.pagination import DefaultPagination
 from .serializers import ProductImageSerializer, ProductSerializer
 from .models import Product, ProductImage
 
 
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['status']
+    search_fields = ['name', 'description']
+    permission_classes = [IsAuthenticated]
     
 class ProductImagesViewSet(ModelViewSet):
     serializer_class = ProductImageSerializer
